017952586129fb8dcb98dd3870922989ef377ade
junxian <757821265@11.com>
2021-10-09 11:00:35 +0800

    异常测试接口

--- src/main/java/com/k8sdevops/demo/controller/MyController.java
+++ src/main/java/com/k8sdevops/demo/controller/MyController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+
 @RestController
 @RequestMapping("/test")
 public class MyController {
@@ -11,5 +13,10 @@ public class MyController {
     @RequestMapping("/a")
     public String test(){
         return "DevopsTest";
+    }
+
+    @RequestMapping("/b")
+    public String fail() throws IOException {
+        throw new IOException("测试异常");
     }
 }
